bf3a995a1528292a1baf42850b292d62de2aefe3
JAVA <JAVA>
2017-02-20 12:27:19 +0100

    añadido Repository a los repositorios y borrado uno de los repositorioAdministrador

--- src/main/java/repositorios/RepositorioCandidato.java
+++ src/main/java/repositorios/RepositorioCandidato.java
@@ -1,9 +1,11 @@
 package repositorios;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import entidades.Candidato;
 
+@Repository
 public interface RepositorioCandidato extends JpaRepository<Candidato, Long> {
 
 }
--- src/main/java/repositorios/RepositorioUsuario.java
+++ src/main/java/repositorios/RepositorioUsuario.java
@@ -2,9 +2,11 @@ package repositorios;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
 
 import entidades.Usuario;
 
+@Repository
 public interface RepositorioUsuario extends JpaRepository<Usuario, Long>{
 	
 	UserDetails findByUsername(String username);
