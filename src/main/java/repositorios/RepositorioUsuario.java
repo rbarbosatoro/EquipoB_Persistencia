0a8fd67cb98bdda554f541e071e2a4e28322210c
María Fernández <mariaomg93@gmail.com>
2017-02-20 12:14:17 +0100

    RepoUsu

---
+++ src/main/java/repositorios/RepositorioUsuario.java
@@ -0,0 +1,12 @@
+package repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import entidades.Usuario;
+
+public interface RepositorioUsuario extends JpaRepository<Usuario, Long>{
+	
+	UserDetails findByUsername(String username);
+
+}
