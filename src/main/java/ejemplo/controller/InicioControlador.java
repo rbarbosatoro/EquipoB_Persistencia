43d2e0f644030222931dba781c52719d53c300d8
Usuario Java <Usuario Java>
2017-02-20 11:36:12 +0100

    controlador inicio dani

--- src/main/java/ejemplo/controller/InicioControlador.java
+++ src/main/java/ejemplo/controller/InicioControlador.java
@@ -1,14 +1,26 @@
 package ejemplo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class InicioControlador {
-	
-	@RequestMapping(value="/")
-	public String index()
-	{
-		return "index";
-	}
+
+		
+//		@Autowired
+//		private repositorioRoles reposiRoles;
+	//	
+		@RequestMapping(value = "/", method = RequestMethod.GET)
+	    public String index(Model model){
+//			
+//			reposiRoles.save(new Permiso((long) 1, "ADMIN"));
+//			reposiRoles.save(new Permiso((long) 2, "CANDIDATO"));
+//			reposiRoles.save(new Permiso((long) 2, "EMPRESA"));
+//			
+			
+			return "index";
+	    }
 }
+
