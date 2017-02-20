aa0c6119dca3e3ae36f7d4843ad1f97747410a10
galp1 <macasu84@gmail.com>
2017-02-20 12:19:36 +0100

    Controladores Empresa Candidato y Oferta cambiados

--- src/main/java/ejemplo/controller/EmpresaControlador.java
+++ src/main/java/ejemplo/controller/EmpresaControlador.java
@@ -2,19 +2,32 @@ package ejemplo.controller;
 
 import javax.validation.Valid;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.empleodigital.domain.repository.UsuarioRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
 
 @Controller
 @RequestMapping("/empresa")
 public class EmpresaControlador {
 
+	@Autowired
+	private EmpresaRepositorio repoEmp;
+	@Autowired
+	private OfertaRepositorio repoOfer;
+
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public String listaEmpresa(Model model)
 	{
@@ -28,6 +41,7 @@ public class EmpresaControlador {
 		model.addAttribute("empresa", repoEmp.findOne(id));
 		return "pages/detalleEmpresa";
 	}
+	
 	@RequestMapping(method=RequestMapping.GET,value="/oferta/{id}")
 	private String detallesOfertaEmpresa (Model model, @PathVariable long id){
 		
@@ -35,18 +49,26 @@ public class EmpresaControlador {
 		return "page/detalleOfertasEmpresa"
 
 	}
+	@RequestMapping(method = RequestMethod.POST)
+	public String guardarEmpresa(Model model, @Valid @ModelAttribute Empresa empresa, BindingResult bindingResult)
+	{
+		empresaRepo.save(empresa);
+		model.addAttribute("empresa",repoEmp.findAll());
+		return "pages/empresaListado";
+	}
 	
 	@RequestMapping(method = RequestMethod.POST)
 	public String guardarOferta(Model model, @Valid @ModelAttribute Oferta oferta, BindingResult bindingResult)
 	{
 		oferRepo.save(oferta);
-		model.addAttribute("oferta",oferRepo.findAll());
+		model.addAttribute("oferta",repoOfer.findAll());
 		return "pages/empresaListado";
 	}
+	
 	@RequestMapping(value =" /{id}", method= RequestMethod.Delete)
 	public ReponseEntity<String> borrarEmpresa(@PathVariable long id){
 		try {
-			empreRepo.delete(id);
+			repoEmp.delete(id);
 			return new ReponseEntity<String>(HttpStatus.OK);
 		} catch (Exception e) {
 			return new ReponseEntity<String>(HttpStatus.BAD_REQUEST);
@@ -61,5 +83,18 @@ public class EmpresaControlador {
 			return new ReponseEntity<String>(HttpStatus.BAD_REQUEST);
 		}
 	}
+	@RequestMapping(method=RequestMethod.GET, value="/{id}")
+	@ResponseBody
+	public Empresa buscarEmpresa(@PathVariable Long id)
+	{
+		Empresa empr =repoEmp.findOne(id);
+		return empr;
+	}
+	
+	@InitBinder
+	public void initBinder(WebDataBinder webDataBinder) {
+		webDataBinder.registerCustomEditor(Empresa.class, empreprop);
+	}
+	
 	
 }
