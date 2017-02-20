eacb4eac71a13099f8943f89cf4e2c84e91c7ac9
Daniel <Daniel>
2017-02-20 12:24:19 +0100

    Añadido datos

--- src/main/java/repositorios/RepositorioCurriculum.java
+++ src/main/java/repositorios/RepositorioCurriculum.java
@@ -1,18 +1,9 @@
 package repositorios;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
 
 import entidades.Curriculum;
 
 public interface RepositorioCurriculum extends JpaRepository<Curriculum, Long>{
 
 }
-=======
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RepositorioCurriculum extends JpaRepository<RepositorioCurriculum, Long>{
-
-}
->>>>>>> 82fef025e9ea9d95f2ed2670a3f970650e0d3dd8
--- src/main/java/repositorios/RepositorioOferta.java
+++ src/main/java/repositorios/RepositorioOferta.java
@@ -1,14 +1,7 @@
 package repositorios;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
 
-import entidades.Oferta;
-
-public interface RepositorioOferta extends JpaRepository<Oferta, Long>{
-
-}
-=======
 import org.springframework.stereotype.Repository;
 
 import entidades.Oferta;
@@ -17,4 +10,4 @@ import entidades.Oferta;
 public interface RepositorioOferta extends JpaRepository<Oferta, Long> {
 
 }
->>>>>>> 82fef025e9ea9d95f2ed2670a3f970650e0d3dd8
+
