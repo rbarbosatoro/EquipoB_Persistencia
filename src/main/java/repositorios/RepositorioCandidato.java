57f775d384c93dfca59342f2fbb41ab05d0ef9f2
JAVA <JAVA>
2017-02-20 11:31:54 +0100

    Correccion de repositoriocandidato

--- src/main/java/repositorios/RepositorioCandidato.java
+++ src/main/java/repositorios/RepositorioCandidato.java
@@ -2,6 +2,6 @@ package repositorios;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RespositorioCandidato extends JpaRepository<Candidato, Long> {
+public interface RepositorioCandidato extends JpaRepository<Candidato, Long> {
 
 }
