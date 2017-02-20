3de0cf984b9c2cf6d00794eab914bb3ad5934cc9
JAVA <JAVA>
2017-02-20 12:02:50 +0100

    Añadido repositorio Administrador

--- src/main/java/repositorios/RepositorioAdministrador.java
+++ src/main/java/repositorios/RepositorioAdministrador.java
@@ -2,6 +2,8 @@ package repositorios;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import entidades.Administrador;
+
 public interface RepositorioAdministrador extends JpaRepository<Administrador, Long> {
 
 }
