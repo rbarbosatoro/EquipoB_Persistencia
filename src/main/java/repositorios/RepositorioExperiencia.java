459f2109130ed0d166a95833ebebdd501fa6a3f2
María Fernández <mariaomg93@gmail.com>
2017-02-20 11:55:30 +0100

    añadido candidato

--- src/main/java/repositorios/RepositorioExperiencia.java
+++ src/main/java/repositorios/RepositorioExperiencia.java
@@ -2,7 +2,8 @@ package repositorios;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import netflix.modelo.entidades.Experiencia;
+import entidades.Experiencia;
+
 
 public interface RepositorioExperiencia extends JpaRepository<Experiencia, Long> {
 
