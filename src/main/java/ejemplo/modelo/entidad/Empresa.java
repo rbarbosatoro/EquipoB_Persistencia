00b2b1a9d243e1516ce0278827da6c42e32182de
Alfredo Fernandez Garcia <alfredofdezgcia@gmail.com>
2017-02-21 12:01:52 +0100

    revision empresa

--- src/main/java/ejemplo/modelo/entidad/Empresa.java
+++ src/main/java/ejemplo/modelo/entidad/Empresa.java
@@ -6,12 +6,7 @@ import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.PrimaryKeyJoinColumn;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.NotEmpty;
