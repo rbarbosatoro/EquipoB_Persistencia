3bdc7945fb00661d784d7f775e8d693898cf4e17
sergio <sergiocaro@yo.yo>
2017-02-23 10:19:39 +0100

    Añadido PerfilEmpresa, DetalleOferta

---
+++ src/main/webapp/static/js/miscript.js
@@ -0,0 +1,73 @@
+$(document).ready(function() {
+
+		$("#editar-datos-empresa").on('click', function() {
+			
+			var id = $(this).parents('tr').data('id');
+			var nombre = $('#empnomb').text();
+			var descripcion = $('#empdescrip').text();
+			var sector = $('#empsect').text();
+			var numempleados = $('#empmunempl').text();
+			var email =	$('#empmail').text();
+			var direccion = $('#empdirec').text();
+			
+			
+			$('#inputId').val(id);
+			$('#nombre-empresa').val(nombre);
+			$('#descripcion-empresa').val(descripcion);
+			$('#sector-empresa').val(sector);
+			$('#numero-empleados').val(numempleados);
+			$('#email-empresa').val(email);
+			$('#direccion-empresa').val(direccion);
+			
+			
+		})
+
+		
+		$("#guardar-cambios").on('click', function() {
+			
+			var id = $('#iddatosempresa').val(id);
+			var nombre = $('#empnomb').text();
+			var descripcion = $('#empdescrip').text();
+			var sector = $('#empsect').text();
+			var numempleados = $('#empmunempl').text();
+			var email =	$('#empmail').text();
+			var direccion = $('#empdirec').text();
+			
+			var datos = {idBBDD: id, nombreBBDD: nombre, descripcionBBDD: descripcion, sectorBBDD: sector, numempleadosBBDD: numempleados, emailBBDD: email, direccionBBDD: direccion };
+			
+			$.post( "rutal del controlador", datos)
+			  .done(function( data ) {
+			    alert( "Data Loaded: " + data );
+			  });
+			
+			
+			$('#editardatosempresa').on('hide.bs.modal', limpiarModal);
+
+		})
+
+		
+		
+		
+
+			
+		$(".borrar-oferta").on("click", function() {
+			var id = $(this).parents('tr').data('id');
+
+			$.ajax({
+				url : "//" + id,
+				
+				type : 'DELETE',
+				 error: function(xhr, status, error) {
+			            alert(status);
+			            alert(this.url);
+			            alert(xhr.responseText);
+			        },
+				success : function(result) {
+					$('tr[data-id="' + id + '"]').remove();
+				}
+			
+			});
+		})
+
+
+});
